0421a1dc546fe7c4e154a2e0da7dbf2a3b0c28bf
DeividVarg <deividjose.v@gmail.com>
2025-06-05 21:30:50 -0400

    add custom domain

--- weib/settings.py
+++ weib/settings.py
@@ -171,8 +171,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Weibstore2024@gmail.com'
 EMAIL_HOST_PASSWORD = 'ajdr dqvr asxc wqqx'
 
-ALLOWED_HOSTS = ['localhost', 'weib-production.up.railway.app','www.weib-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost', 'weib-production.up.railway.app','www.weib-production.up.railway.app', 'weibstore.com', 'www.weibstore.com']
 CSRF_TRUSTED_ORIGINS = [
     'https://weib-production.up.railway.app',
+    'weibstore.com', 'www.weibstore.com'
 ]
 
